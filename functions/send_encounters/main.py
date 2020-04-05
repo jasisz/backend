a96c83f992d00453f164a5e0f57f8bf2e0602132
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2020-04-05 18:39:10 +0200

    [WIP] prototyp uswspólnienia kodu między funkcjami

--- functions/send_encounters/main.py
+++ functions/send_encounters/main.py
@@ -9,15 +9,13 @@ from flask import jsonify, Request
 from google.cloud import bigquery, datastore
 from google.cloud.datastore import Entity
 
+from utils import get_request_data, validate_request_parameters, KEY_USER_ID, KEY_PLATFORM, KEY_OS_VERSION, \
+    KEY_DEVICE_TYPE, KEY_APP_VERSION, KEY_LANG, get_user_from_datastore, update_user_in_datastore, \
+    InvalidRequestException, ExtraParam
+
 BQ_TABLE_ID = f"{os.environ['GCP_PROJECT']}.{os.environ['BQ_DATASET']}.{os.environ['BQ_TABLE']}"
 USERS_DATASTORE_KIND = "Users"
 ENCOUNTER_UPLOADS_DATASTORE_KIND = "Encounter Uploads"
-KEY_USER_ID = "user_id"
-KEY_PLATFORM = "platform"
-KEY_OS_VERSION = "os_version"
-KEY_DEVICE_TYPE = "device_type"
-KEY_APP_VERSION = "app_version"
-KEY_LANG = "lang"
 KEY_ENCOUNTERS = "encounters"
 KEY_ENCOUNTER_DATE = "encounter_date"
 KEY_BEACON_ID = "beacon_id"
@@ -25,11 +23,6 @@ KEY_SIGNAL_STRENGTH = "signal_strength"
 
 datastore_client = datastore.Client()
 
-
-class InvalidRequestException(Exception):
-    def __init__(self, status, response):
-        self.status = status
-        self.response = response
 
 
 def send_encounters(request):
@@ -46,10 +39,13 @@ def send_encounters(request):
     lang = request_data[KEY_LANG]
     encounters = request_data[KEY_ENCOUNTERS]
 
-    user_entity = _get_user_entity(user_id)
-
-    if not user_entity:
-        return jsonify({"status": "failed", "message": "unauthorized"}), 401
+    try:
+        user_entity = get_user_from_datastore(user_id)
+    except InvalidRequestException as e:
+        return jsonify(
+            e.response,
+            e.status
+        )
 
     upload_id = secrets.token_hex(32)
 
@@ -57,21 +53,13 @@ def send_encounters(request):
     if not _save_encounters_to_bigquery(user_id, upload_id, encounters):
         return jsonify({"status": "failed", "message": "Internal error"}), 500
 
-    _update_user_entity(user_entity, platform, os_version, app_version, device_type, lang)
+    update_user_in_datastore(user_entity, platform, os_version, app_version, device_type, lang)
     return jsonify({"status": "OK"})
 
 
 def _parse_request(request: Request) -> dict:
-    if not request.is_json:
-        raise InvalidRequestException(422, {"status": "failed", "message": "invalid data"})
-
-    request_data = request.get_json()
-
-    for key in [KEY_USER_ID, KEY_PLATFORM, KEY_OS_VERSION, KEY_DEVICE_TYPE, KEY_APP_VERSION, KEY_LANG, KEY_ENCOUNTERS]:
-        if key not in request_data:
-            raise InvalidRequestException(422, {"status": "failed", "message": f"missing field: {key}"})
-        if not request_data[key]:
-            raise InvalidRequestException(422, {"status": "failed", "message": f"empty field: {key}"})
+    request_data = get_request_data(request)
+    validate_request_parameters(request_data, extra_params=[ExtraParam(KEY_ENCOUNTERS)])
 
     for encounter in request_data[KEY_ENCOUNTERS]:
         for key in [KEY_ENCOUNTER_DATE, KEY_BEACON_ID, KEY_SIGNAL_STRENGTH]:
@@ -92,15 +80,13 @@ def _save_encounter_uploads_to_datastore(user_id: str, upload_id: str) -> None:
     uploads_key = datastore_client.key(ENCOUNTER_UPLOADS_DATASTORE_KIND, f"{upload_id}")
     entity = datastore.Entity(key=uploads_key)
 
-    entity.update(
-        {
-            "user_id": user_id,
-            "upload_id": upload_id,
-            "date": datetime.now(tz=pytz.utc),
-            "processed_by_health_authority": False,
-            "confirmed_by_health_authority": None,
-        }
-    )
+    entity.update({
+        'user_id': user_id,
+        'upload_id': upload_id,
+        'date': datetime.utcnow(),
+        'processed_by_health_authority': False,
+        'confirmed_by_health_authority': None,
+    })
     datastore_client.put(entity)
 
 
@@ -110,30 +96,24 @@ def _save_encounters_to_bigquery(user_id: str, upload_id: str, encounters: list)
 
     rows_to_insert = []
     for encounter in encounters:
-        encounter_date = datetime.strptime(encounter[KEY_ENCOUNTER_DATE], "%Y%m%d%H").replace(tzinfo=pytz.utc)
+        encounter_date = datetime.strptime(encounter[KEY_ENCOUNTER_DATE], "%Y%m%d%H")
         beacon_id = encounter[KEY_BEACON_ID]
         signal_strength = encounter[KEY_SIGNAL_STRENGTH]
-        rows_to_insert.append((upload_id, user_id, encounter_date, beacon_id, signal_strength))
+        rows_to_insert.append(
+            (
+                upload_id,
+                user_id,
+                encounter_date,
+                beacon_id,
+                signal_strength
+            )
+        )
 
     errors = bq_client.insert_rows(table, rows_to_insert)
     if errors:
-        logging.error(f"Unable to save encounters for user_id: {user_id}")
+        logging.error(f'Unable to save encounters for user_id: {user_id}')
         for e in errors:
             logging.error(e)
         return False
 
     return True
-
-
-def _update_user_entity(entity: Entity, platform: str, os_version: str, app_version: str, device_type: str, lang: str) -> None:
-    entity.update(
-        {
-            "platform": platform,
-            "os_version": os_version,
-            "app_version": app_version,
-            "device_type": device_type,
-            "lang": lang,
-            "last_status_requested": datetime.now(tz=pytz.utc),
-        }
-    )
-    datastore_client.put(entity)
---
+++ utils.py
@@ -0,0 +1,109 @@
+from _ctypes import Array
+from datetime import datetime
+from typing import List
+
+from flask import Request
+from google.cloud import datastore
+from google.cloud.datastore import Entity
+
+DATASTORE_KIND_USERS = "Users"
+ENCOUNTER_UPLOADS_DATASTORE_KIND = "Encounter Uploads"
+
+KEY_USER_ID = "user_id"
+KEY_PLATFORM = "platform"
+KEY_OS_VERSION = "os_version"
+KEY_DEVICE_TYPE = "device_type"
+KEY_APP_VERSION = "app_version"
+KEY_LANG = "lang"
+
+datastore_client = datastore.Client()
+
+
+class InvalidRequestException(Exception):
+    def __init__(self, status, response):
+        self.status = status
+        self.response = response
+
+
+class ExtraParam():
+    def __init__(self, name, allow_empty=False):
+        self.name = name
+        self.allow_empty = allow_empty
+
+
+def get_request_data(request: Request) -> dict:
+    if not request.is_json:
+        raise InvalidRequestException(
+            422,
+            {
+                'status': 'failed',
+                'message': 'invalid data'
+            }
+        )
+
+    return request.get_json()
+
+
+def validate_request_parameters(request_data: dict, extra_params: List[ExtraParam] = None) -> None:
+    for key in [KEY_USER_ID, KEY_PLATFORM, KEY_OS_VERSION, KEY_DEVICE_TYPE, KEY_APP_VERSION, KEY_LANG]:
+        if key not in request_data:
+            raise InvalidRequestException(
+                422,
+                {
+                    'status': 'failed',
+                    'message': f'missing field: {key}'
+                }
+            )
+        if not request_data[key]:
+            raise InvalidRequestException(
+                422,
+                {
+                    'status': 'failed',
+                    'message': f'empty field: {key}'
+                }
+            )
+
+    for param in extra_params:
+        if param.name not in request_data:
+            raise InvalidRequestException(
+                422,
+                {
+                    'status': 'failed',
+                    'message': f'missing field: {key}'
+                }
+            )
+        if not param.allow_empty and not request_data[param.name]:
+            raise InvalidRequestException(
+                422,
+                {
+                    'status': 'failed',
+                    'message': f'empty field: {key}'
+                }
+            )
+
+
+def get_user_from_datastore(user_id: str) -> Entity:
+    key = datastore_client.key(DATASTORE_KIND_USERS, f"{user_id}")
+    user_entity = datastore_client.get(key=key)
+    if not user_entity:
+        raise InvalidRequestException(
+            401,
+            {
+                'status': 'failed',
+                'message': f'Unauthorized'
+            }
+        )
+    return user_entity
+
+
+def update_user_in_datastore(entity: Entity, platform: str, os_version: str, app_version: str, device_type: str,
+                             lang: str) -> None:
+    entity.update({
+        "platform": platform,
+        "os_version": os_version,
+        "app_version": app_version,
+        "device_type": device_type,
+        "lang": lang,
+        "last_status_requested": datetime.utcnow(),
+    })
+    datastore_client.put(entity)
